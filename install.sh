03cd8513d29d75751f49d0ec883c97bc6fa54c73
Manuel Spagnolo <shikaan@users.noreply.github.com>
2023-02-18 00:26:16 +0100

    docs: add installation scripts (#4)
    * docs: add installation scripts
    
    * refactor: make install script posix compliant
    
    * docs: update readme with installation instructions

--- install.sh
+++ install.sh
@@ -1,18 +1,37 @@
-#!/bin/bash
+#!/bin/env sh
 
-OS=$(uname | tr '[:upper:]' '[:lower:]')
-ARCH=$([[ $(uname -m) == "x86_64" ]] && echo "amd64" || echo "386")
+get_architecture () {
+  arch="$(uname -m)"
 
-has_wget=$(which wget)
+  if [ "$arch" = "x86_64" ]; then
+    echo "am64"
+  else
+    echo "$arch"
+  fi
+}
 
-echo "Installing the binary..."
-if [ -z has_wget ]; then
-  curl -sfLo /usr/local/bin/keydex https://github.com/shikaan/keydex/releases/latest/download/keydex-${OS}-${ARCH}
-else
-  wget -q -O /usr/local/bin/keydex https://github.com/shikaan/keydex/releases/latest/download/keydex-${OS}-${ARCH}
+get_os () {
+  uname | tr '[:upper:]' '[:lower:]'
+}
+
+OS=$(get_os)
+ARCH=$(get_architecture)
+HAS_WGET=$(which wget)
+
+if [ "$(id -u)" -ne 0 ]; then 
+  echo "Installation needs to be run as super user. Please run 'sudo $0' to proceed."
+  exit 1
 fi
 
+echo "> Downloading..."
+if [ -z "$HAS_WGET" ]; then
+  curl -sfLo /usr/local/bin/keydex https://github.com/shikaan/keydex/releases/latest/download/keydex-"${OS}"-"${ARCH}"
+else
+  wget -q -O /usr/local/bin/keydex https://github.com/shikaan/keydex/releases/latest/download/keydex-"${OS}"-"${ARCH}"
+fi
+
+echo "> Setting permissions..."
 chmod u+x /usr/local/bin/keydex
-chown $(logname) /usr/local/bin/keydex
+chown "$(logname)" /usr/local/bin/keydex
 
 echo "Done!"
--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,10 @@
 #!/bin/env sh
 
+if [ "$(id -u)" -ne 0 ]; then 
+  echo "Uninstallation needs to be run as super user. Please run 'sudo $0' to proceed."
+  exit 1
+fi
+
+echo "Removing previous installation..."
 rm /usr/local/bin/keydex
+echo "Done!"
