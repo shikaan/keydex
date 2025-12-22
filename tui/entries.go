b81bd64f8a32601b71389e224ba55a0f37075b9d
Manuel Spagnolo <manuel.spagnolo@proton.me>
2025-12-22 17:04:56 +0100

    feat: delete entries from entry list

--- tui/entries.go
+++ tui/entries.go
@@ -1,22 +1,81 @@
 package tui
 
 import (
+	"fmt"
 	"math"
 
 	"github.com/gdamore/tcell/v2"
 	"github.com/gdamore/tcell/v2/views"
 
+	"github.com/shikaan/keydex/pkg/log"
 	"github.com/shikaan/keydex/tui/components"
 	"github.com/shikaan/keydex/tui/components/autocomplete"
 )
 
-type ListView struct {
+type EntriesView struct {
+	autoComplete *autocomplete.AutoComplete
 	components.Container
+}
+
+func (lv *EntriesView) HandleEvent(ev tcell.Event) bool {
+	switch ev := ev.(type) {
+	case *tcell.EventKey:
+		if ev.Name() == "Ctrl+D" {
+			if App.State.IsReadOnly {
+				msg := "Could not delete. Archive in read-only mode."
+				App.Notify(msg)
+				log.Info(msg)
+				return true
+			}
+
+			entry := App.State.Database.GetFirstEntryByPath(lv.autoComplete.CurrentEntry)
+			if entry == nil {
+				msg := "Could not delete. Group cannot be found."
+				App.Notify(msg)
+				log.Error(msg, nil)
+			}
+
+			title := entry.GetTitle()
+
+			App.Confirm(
+				"Are you sure you want to delete \""+title+"\"?",
+				func() {
+					err := App.State.Database.RemoveEntry(entry.UUID)
+					if err != nil {
+						msg := "Could not delete. Entry cannot be found."
+						App.Notify(msg)
+						log.Error(msg, err)
+						return
+					}
+
+					if e := App.State.Database.SaveAndUnlockEntries(); e != nil {
+						App.State.IsReadOnly = true
+						msg := "Could not save. Switching to read-only to not corrupt data."
+						App.Notify(msg)
+						log.Error(msg, e)
+						return
+					}
+
+					msg := fmt.Sprintf("Entry \"%s\" deleted successfully.", title)
+					App.Notify(msg)
+					log.Info(msg)
+
+					App.NavigateTo(NewEntryListView)
+				}, func() {
+					msg := "Operation cancelled. Entry was not deleted."
+					App.Notify(msg)
+					log.Info(msg)
+				},
+			)
+		}
+	}
+
+	return lv.autoComplete.HandleEvent(ev)
 }
 
 func NewEntryListView(screen tcell.Screen) views.Widget {
 	App.SetTitle("Search")
-	view := &ListView{}
+	view := &EntriesView{}
 	view.Container = *components.NewContainer(screen)
 	paths := App.State.Database.GetEntryPaths()
 	count := len(paths)
@@ -44,6 +103,7 @@ func NewEntryListView(screen tcell.Screen) views.Widget {
 	})
 	autoComplete.SetFocus(true)
 	view.SetContent(autoComplete)
+	view.autoComplete = autoComplete
 
 	return view
 }
